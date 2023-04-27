75f571ec97d6432c77759e624a50e778e737993a
Sheldon <131889712+SheldonFung@users.noreply.github.com>
2023-04-27 15:13:13 -0400

    Update poeme.cs

--- poeme.cs
+++ poeme.cs
@@ -4,6 +4,8 @@ class Poeme1
 {
   static void main()
   {
-  Console.WriteLine("Le ciel est gris")
+  Console.WriteLine("Le ciel est gris");
+    Console.WriteLine("Il pleut un peu");
+    Console.WriteLine("j'ai mal à tête");
   }
 }
